f99aa3426705599f83eadd8b86bedfc081ffb000
Lieven Merckx <lieven.merckx@ing.com>
2024-10-18 10:31:38 +0200

    sync

--- Inc/limero/codec.h
+++ Inc/limero/codec.h
@@ -12,127 +12,155 @@
 typedef bool Void;
 
 template <typename T>
-class Option {
+class Option
+{
 private:
     bool _has_value = true;
-    T* _value_ptr;
+    T _value;
+
+    Option(bool has_value, T value) : _has_value(has_value), _value(value) {}
+
 
 public:
-    static Option<T> None() {
-        Option<T> option = {
-             false,
-             NULL,
-        };
-        return option;
+    Option() : _has_value(false);
+    static Option None()
+    {
+        return Option{};
     }
-    static Option<T> Some(T value) {
-        T* value_ptr = new T(value);
-        Option<T> option = Option<T>{
-             true,
-             value_ptr,
-        };
-        return option;
+    void none() { _has_value = false ; }
+    void some(T value) { _has_value = true; _value = value; }
+
+    static Option Some(T value)
+    {
+        return Option{true, value};
     }
-    //    Option(T t) : _has_value(false), _value(t) {}
-    //    Option(bool none) : _has_value(none) {}
-    ~Option() {
-        if (_has_value) {
-            delete _value_ptr;
-        }
+
+    ~Option()
+    {
+       
     }
     bool is_some() { return !_has_value; }
     bool is_none() { return _has_value; }
     T unwrap() { return *_value_ptr; }
-    void inspect(std::function<void(T)> ff) {
-        if (_has_value) {
-            ff(*_value_ptr);
+    void inspect(std::function<void(T)> ff)
+    {
+        if (_has_value)
+        {
+            ff(_value);
         }
     }
     template <typename U>
-    Option<U> map(std::function<Option<U>(T)> ff) {
-        if (_has_value) {
-            return ff(*_value_ptr);
+    Option<U> map(std::function<Option<U>(T)> ff)
+    {
+        if (_has_value)
+        {
+            return ff(_value;
         }
-        else {
-            return Option<U>::None();
-        }
+        return Option<U>::None();
     }
 };
 
 template <typename T>
-class Result {
+class Result
+{
     T _value;
     int _err;
-    const char* _msg;
+    const char *_msg;
 
 public:
     Result() : _value(), _err(0) {}
     bool is_ok() { return _err == 0; }
     bool is_err() { return _err != 0; }
-    const char* get_err_msg() { return (_err == 0) ? "No error" : _msg; }
-    Result& on_error(std::function<void(const char*)> ff) {
-        if (_err != 0) {
+    const char *get_err_msg() { return (_err == 0) ? "No error" : _msg; }
+    Result &on_error(std::function<void(const char *)> ff)
+    {
+        if (_err != 0)
+        {
             ff(_msg);
         }
         return *this;
     }
-    Result& on_ok(std::function<void(T)> ff) {
-        if (_err == 0) {
+    Result &on_ok(std::function<void(T)> ff)
+    {
+        if (_err == 0)
+        {
             ff(_value);
         }
-        else {
+        else
+        {
             ERROR("Error: %d\n", _err);
         }
         return *this;
     }
-    T unwrap() {
-        if (_err != 0) {
+    T unwrap()
+    {
+        if (_err != 0)
+        {
             ERROR("Error: %d\n", _err);
         }
         return _value;
     }
-    static Result<T> Ok(T value) {
+    static Result<T> Ok(T value)
+    {
         Result<T> result;
         result._value = value;
         result._err = 0;
         return result;
     }
-    static Result<T> Err(int err) {
+    void ok(T value) {
+        _value = value;
+        _err = 0;
+        _msg = "No error"
+    }
+    static Result<T> Err(int err)
+    {
         Result<T> result;
         result._err = err;
         result._msg = strerror(err);
         return result;
     }
-    static Result<T> Err(int err, const char* msg) {
+    void err(int err, const char *msg = "unknown failure") {
+        _err =  err;
+        _msg = msg;
+    }
+    static Result<T> Err(int err, const char *msg)
+    {
         Result<T> result;
         result._err = err;
         result._msg = msg;
         return result;
     }
-    void inspect(std::function<void(T)> ff) {
-        if (_err == 0) {
+    void inspect(std::function<void(T)> ff)
+    {
+        if (_err == 0)
+        {
             ff(_value);
         }
     }
     template <typename U>
-    Result<U> map(std::function<Result<U>(T)> ff) {
-        if (_err == 0) {
+    Result<U> map(std::function<Result<U>(T)> ff)
+    {
+        if (_err == 0)
+        {
             return ff(_value);
         }
         return Result<U>::Err(this->_err, this->_msg);
     }
 };
 
-#define RET_ERR(x)      \
-    if ((x).is_err()) { \
-        return x;       \
+#define RET_ERR(x)    \
+    if ((x).is_err()) \
+    {                 \
+        return x;     \
     }
-#define RET_ER(x)       \
-    if ((x).is_err()) { \
-        return;         \
+#define RET_ER(x)     \
+    if ((x).is_err()) \
+    {                 \
+        return;       \
     }
 
-class FrameEncoder {
+class FrameEncoder
+{
 private:
     std::vector<uint8_t> _buffer;
     uint32_t _max;
@@ -146,24 +174,25 @@ public:
     Result<Void> end_map();
     Result<Void> add_map(int8_t key, int32_t value);
     Result<Void> encode_uint32(uint32_t input_value);
-    Result<Void> encode_str(const char* str);
-    Result<Void> encode_bstr(std::vector<uint8_t>& buffer);
+    Result<Void> encode_str(const char *str);
+    Result<Void> encode_bstr(std::vector<uint8_t> &buffer);
     Result<Void> encode_float(float value);
     Result<Void> encode_int32(int32_t value);
     Result<Void> encode_bool(bool b);
     Result<Void> encode_null();
     Result<Void> add_crc();
     Result<Void> add_cobs();
-    Result<Void> read_buffer(uint8_t* buffer, size_t len);
-    Result<Void> read_buffer(std::vector<unsigned char>& buffer);
+    Result<Void> read_buffer(uint8_t *buffer, size_t len);
+    Result<Void> read_buffer(std::vector<unsigned char> &buffer);
     Result<Void> clear();
     Result<Void> rewind();
     Result<std::string> to_string();
-    uint8_t* data() { return _buffer.data(); }
+    uint8_t *data() { return _buffer.data(); }
     uint32_t size() { return _buffer.size(); }
 };
 
-enum CborType {
+enum CborType
+{
     CBOR_UINT32 = 0,
     CBOR_STR = 1,
     CBOR_FLOAT = 2,
@@ -172,7 +201,8 @@ enum CborType {
     CBOR_BSTR = 5,
 };
 
-class FrameDecoder {
+class FrameDecoder
+{
 private:
     std::vector<uint8_t> _buffer;
     uint32_t _index;
@@ -199,22 +229,24 @@ public:
     Result<Void> decode_cobs();
     Result<bool> add_byte(uint8_t byte);
     Result<bool> fill_buffer(std::vector<uint8_t> buffer);
-    Result<bool> fill_buffer(uint8_t* buffer, uint32_t size);
-    Result<Void> read_buffer(uint8_t* buffer, size_t len);
-    Result<Void> read_buffer(std::vector<unsigned char>& buffer);
+    Result<bool> fill_buffer(uint8_t *buffer, uint32_t size);
+    Result<Void> read_buffer(uint8_t *buffer, size_t len);
+    Result<Void> read_buffer(std::vector<unsigned char> &buffer);
     Result<Void> clear();
     Result<Void> rewind();
     Result<std::string> to_string();
 };
 
 // FNV-1a hash function for 32-bit hash value
-constexpr uint32_t fnv1a_32_1(const char* str, uint32_t hash = 2166136261U) {
+constexpr uint32_t fnv1a_32_1(const char *str, uint32_t hash = 2166136261U)
+{
     return *str == '\0' ? hash : fnv1a_32_1(str + 1, (hash ^ static_cast<uint32_t>(*str)) * 16777619U);
 }
 
 // Helper to compute the hash at compile time for a string literal
 template <std::size_t N>
-constexpr uint32_t FNV(const char(&str)[N]) {
+constexpr uint32_t FNV(const char (&str)[N])
+{
     return fnv1a_32_1(str);
 }
 #endif
